93677953f6ab5e5f037325dd3101ee26dc0e283a
ribnil <ribnil>
2020-05-21 10:02:21 +0900

    World Teleporter

--- src/world_manager/forms/WorldTeleportForm.php
+++ src/world_manager/forms/WorldTeleportForm.php
@@ -4,7 +4,18 @@ declare(strict_types=1);
 
 namespace world_manager\forms;
 
-class WorldTeleportForm
-{
+use pocketmine\Player;
+use world_manager\WorldManager;
+use world_manager\forms\template\ButtonForm;
 
+class WorldTeleportForm extends ButtonForm
+{
+	public function __construct(WorldManager $worldManager) {
+		$handler = $worldManager->getWorldHandler();
+		$worlds = $handler->getAllWorldName();
+		parent::__construct(function(Player $player, $data) use($handler, $worlds): void {
+			if($data !== null)
+				$handler->shiftWorld($player, $worlds[$data]);
+		}, "World Teleporter.", "warp to...", $worlds);
+	}
 }
